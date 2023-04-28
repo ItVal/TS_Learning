72c87e896ba379d218513c0e461aec87fa637438
ItVal <valentinhonvj1@gmail.com>
2023-04-28 16:09:16 +0200

    feat object type in ts

--- Syntaxe.js
+++ Syntaxe.js
@@ -3,3 +3,9 @@ function addition(x, y) {
 }
 var result = addition(10, 20);
 console.log(result);
+function damage(characterToDamage, amount) {
+    characterToDamage.life -= amount;
+    return characterToDamage.life;
+}
+var res = damage({ life: 100 }, 12);
+console.log(res);
--- Syntaxe.ts
+++ Syntaxe.ts
@@ -3,3 +3,11 @@ function addition(x: number, y: number): number {
 }
 const result:number = addition(10, 20);
 console.log(result);
+
+
+function damage(characterToDamage: {life:number}, amount: number): number {
+    characterToDamage.life -= amount;
+    return characterToDamage.life;
+}
+const res = damage({ life: 100 }, 12);
+console.log(res);
