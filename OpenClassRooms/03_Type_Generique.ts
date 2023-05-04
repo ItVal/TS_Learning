1deed476cda39268a8b3196e5ca08183656c7dfb
ItVal <valentinhonvj1@gmail.com>
2023-05-04 16:53:24 +0200

    feat call Equipement type in equipements field

--- OpenClassRooms/03_Type_Generique.ts
+++ OpenClassRooms/03_Type_Generique.ts
@@ -6,10 +6,18 @@ type Character = {
  };
  
  type Pet = Character;
-
+ type Equipment = {
+    price: number;
+    attack?: number;
+    defense?: number;
+};
 //supposons un type shop
 type Shop = {
     name: string;
     owner: Character; // Le même "Character" qu'on a vu au chapitre précédent
-    items: Array<unknown>; //unknown signifie que ce champs peut accepter un tableau qui peux accepter n'importe quel type
+    equipements: Array<unknown>; //unknown signifie que ce champ équipement peut accepter un tableau qui peux accepter n'importe quel type
+};
+//donnons ce champs équipements, le type Equipement
+type Armory = Shop & { //le type Armory contient desormais le type shop et on précise évidement le type de noms équipement de (Shop) = Equipement
+    equipements: Array<Equipment>;
 };
